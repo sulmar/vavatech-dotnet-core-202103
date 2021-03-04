27193b47ac476560a8f5bf533d3b816c7966f26a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-04 17:07:01 +0100

    TagHelpers and ViewComponents

---
+++ Vavatech.DotnetCore.MVCWebApp/Components/CustomerCardViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vavatech.DotnetCore.Models;
+
+namespace Vavatech.DotnetCore.MVCWebApp.Components
+{
+    public class CustomerCardViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Customer customer)
+        {
+            return View(customer);
+        }
+    }
+}
---
+++ Vavatech.DotnetCore.MVCWebApp/TagHelpers/CurrentTimeTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vavatech.DotnetCore.MVCWebApp.TagHelpers
+{
+    public class CurrentTimeTagHelper : TagHelper
+    {
+        [HtmlAttributeName("project-title")]
+        public string Text { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string html = $"<b>{Text}</b>";
+
+            output.Attributes.SetAttribute("class", "mb-4");
+            output.TagName = "div";
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
