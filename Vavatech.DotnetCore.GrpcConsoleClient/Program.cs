68abd1e1383a41d76e9b5d8ed999dbd5ffc3f2b3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-05 16:54:48 +0100

    grpc

---
+++ Vavatech.DotnetCore.GrpcConsoleClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Bogus;
+using Grpc.Net.Client;
+using System;
+using System.Threading;
+using Tracking.API;
+
+namespace Vavatech.DotnetCore.GrpcConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello gRPC Client!");
+
+            const string url = "https://localhost:5001";
+
+            var locations = new Faker<AddLocationRequest>()
+                .RuleFor(p => p.DeviceId, f => f.Internet.Mac())
+                .RuleFor(p => p.Latitude, f => f.Random.Float(-90f, 90f))
+                .RuleFor(p => p.Longitude, f => f.Random.Float(-180f, 180f))
+                .RuleFor(p => p.Speed, f => f.Random.Int(0, 140))
+                .RuleFor(p=>p.Direction, f=>f.Random.Float())
+                .GenerateForever();
+
+            var channel = GrpcChannel.ForAddress(url);
+            var client = new Tracking.API.TrackingService.TrackingServiceClient(channel);
+
+            foreach (var location  in locations)
+            {
+                client.AddLocation(location);
+
+                Console.WriteLine($"Sent {location.DeviceId} {location.Latitude}:{location.Longitude} {location.Speed} {location.Direction}");
+
+                Thread.Sleep(TimeSpan.FromSeconds(0.01));
+            }
+
+          
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Vavatech.DotnetCore.GrpcService/Services/MyTrackingService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tracking.API;
+
+namespace Vavatech.DotnetCore.GrpcService.Services
+{
+    public class MyTrackingService : Tracking.API.TrackingService.TrackingServiceBase
+    {
+        private readonly ILogger<MyTrackingService> logger;
+
+        public MyTrackingService(ILogger<MyTrackingService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<AddLocationResponse> AddLocation(AddLocationRequest request, ServerCallContext context)
+        {
+            logger.LogInformation($"{request.DeviceId} {request.Latitude}:{request.Longitude} {request.Speed} {request.Direction}");
+
+            var response = new AddLocationResponse { IsConfirmed = true };
+
+            return Task.FromResult(response);
+        
+        }
+    }
+}
