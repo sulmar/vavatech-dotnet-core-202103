175d9d7ac5a369ec9a255382963accb7305c75a2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-05 15:38:44 +0100

    Blazor

---
+++ Vavatech.DotnetCore.BlazorServerClient/Pages/Customers.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vavatech.DotnetCore.IServices;
+using Vavatech.DotnetCore.Models;
+
+namespace Vavatech.DotnetCore.BlazorServerClient.Pages
+{
+    public partial class Customers : ComponentBase
+    {
+        [Inject]
+        protected ICustomerService CustomerService { get; set; }
+
+        protected IEnumerable<Customer> customers;
+
+        protected override void OnInitialized()
+        {
+            customers = CustomerService.Get();
+        }
+    }
+}
